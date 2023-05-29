27c13f190523b84ab63e7830f4a237b0c4e74382
seiya.miura <seiya.miura@g.dreamcareer.co.jp>
2023-05-29 15:27:49 +0900

    GoodsDao->insertDb関数の追加

--- GoodsManagement/src/main/java/com/example/demo/db/GoodsDao.java
+++ GoodsManagement/src/main/java/com/example/demo/db/GoodsDao.java
@@ -1,5 +1,25 @@
 package com.example.demo.db;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class GoodsDao {
 
+	private final JdbcTemplate db;
+	
+	@Autowired
+	public GoodsDao(JdbcTemplate db) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.db=db;
+	}
+	
+	public void insertDb(Goods goods) {
+		db.update("INSERT INTO goods (name,stock,category,price) VALUES(?,?,?)", 
+				goods.getName(),
+				goods.getStock(), 
+				goods.getCategoly(),
+				goods.getPrice());
+	}
 }
---
+++ GoodsManagement/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+--カテゴリー追加
+--INSERT INTO category(id,name) VALUES(0,'aaa')
+
+
+--商品追加
+INSERT INTO goods(name,stock,category,price) VALUES
+('AAAsample',1000,'aaaaaaaaaa',21321),
+('BBBsample',2000,'bbbbbbbbbb',133),
+('CCCsample',2525,'cccccccccc',5543);
+
