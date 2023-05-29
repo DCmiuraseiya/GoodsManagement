7f1e7c471c990820aaba8f8357dd936208d97cfb
seiya.miura <seiya.miura@g.dreamcareer.co.jp>
2023-05-29 17:11:23 +0900

    editの追加

--- GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
+++ GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
@@ -2,7 +2,13 @@ package com.example.demo.form;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.db.Goods;
 import com.example.demo.db.GoodsDao;
 
 @Controller
@@ -14,4 +20,28 @@ public class GoodsController {
 		// TODO 自動生成されたコンストラクター・スタブ
 		this.goodsdao=goodsdao;
 	}
+	
+	@RequestMapping("/edit/{id}")
+	public String edit(@PathVariable Long id,Model model) {
+		Goods goods=goodsdao.serchDbOne(id);
+		model.addAttribute("goods",goods);
+		return "/goods/edit";
+	}
+	
+	@RequestMapping("/edit/{id}/exe")
+    public String editExe(@PathVariable Long id, @Validated Form form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("edit_title", "編集画面");
+            model.addAttribute("carender", "カレンダーを入力してください");
+            return "diary/edit";
+        }
+        Goods goods = new Goods();
+        goods.setStock(form.getStock());
+        goods.setName(form.getName());
+        goods.setCategory(form.getCategory());
+        goods.setPrice(form.getPrice());
+        goodsdao.updateDb(id, goods);
+        return "redirect:/index";
+	}
 }
+
