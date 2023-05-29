fcbc7f4e6e31b051b31658e266ca619939a148d7
大久保 <kou.okubo@g.dreamcareer.co.jp>
2023-05-29 16:54:40 +0900

    add complete complete.html

--- GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
+++ GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
@@ -1,5 +1,30 @@
 package com.example.demo.form;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.db.Goods;
+import com.example.demo.db.GoodsDao;
+
 public class GoodsController {
 
+	private final GoodsDao goodsdao;
+
+	@Autowired
+	public GoodsController(GoodsDao goodsdao) {
+		this.goodsdao = goodsdao;
+	}
+
+	//確認
+	@RequestMapping("/complete")
+	public String complete(Model model, Form form) {
+		model.addAttribute("title", "完了ページ");
+		Goods goods = new Goods();
+		goods.setName(form.getName());
+		goods.setStock(form.getStock());
+		goods.setCategory(form.getCategory());
+		goodsdao.insertDb(goods);
+		return "/complete";
+	}
 }
