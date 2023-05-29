77bdc950f6b8d457af842adc4d2252542d3bf267
seiya.miura <seiya.miura@g.dreamcareer.co.jp>
2023-05-29 16:12:51 +0900

    エラーの修正

--- GoodsManagement/src/main/java/com/example/demo/db/GoodsDao.java
+++ GoodsManagement/src/main/java/com/example/demo/db/GoodsDao.java
@@ -50,13 +50,13 @@ public class GoodsDao {
 		db.update("INSERT INTO goods (name,stock,category,price) VALUES(?,?,?)", 
 				goods.getName(),
 				goods.getStock(), 
-				goods.getCategoly(),
+				goods.getCategory(),
 				goods.getPrice());
 	}
 	
 	public void updateDb(Long id, Goods goods) {
 		//コンソールに表示
 		System.out.println("編集の実行");
-		db.update("UPDATE goods SET name=?, stock=?, categoly=?, price=? WHERE id=?", goods.getName(), goods.getStock(), goods.getCategoly(), goods.getPrice(), id);
+		db.update("UPDATE goods SET name=?, stock=?, categoly=?, price=? WHERE id=?", goods.getName(), goods.getStock(), goods.getCategory(), goods.getPrice(), id);
 	}
 }
