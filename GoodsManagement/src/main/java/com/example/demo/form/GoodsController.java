fea77c33c5278f9b4adcb5d9831a3f38189d700b
seiya.miura <seiya.miura@g.dreamcareer.co.jp>
2023-05-29 16:43:29 +0900

    controllerのひな型の作成

--- GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
+++ GoodsManagement/src/main/java/com/example/demo/form/GoodsController.java
@@ -1,5 +1,17 @@
 package com.example.demo.form;
 
-public class GoodsController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.example.demo.db.GoodsDao;
+
+@Controller
+public class GoodsController {
+	private final GoodsDao goodsdao;
+	@Autowired
+	public GoodsController(GoodsDao goodsdao) {
+		
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.goodsdao=goodsdao;
+	}
 }
