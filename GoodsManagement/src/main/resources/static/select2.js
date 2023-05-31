a39e4149f218ef796b6bc9fb4ba3cc5c8284f79d
khitoshi <95644312+Khitoshi@users.noreply.github.com>
2023-05-31 14:30:54 +0900

    Add files via upload
    select2用js

---
+++ GoodsManagement/src/main/resources/static/select2.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.select').select2({
+    width: '200px',
+    allowClear: true,
+    // Select2メッセージに使用する言語を指定します。
+    language: 'ja',
+    tags: true
+  });
+
+});
